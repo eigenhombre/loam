59b37b39ec388be5cba9e7037589d7afb4024074
John Jacobsen <eigenhombre@gmail.com>
2019-09-20 21:06:25 -0500

    A little guy / gal moves around.  A few things exist here and there.

--- src/main.lisp
+++ src/main.lisp
@@ -6,31 +6,90 @@
 ;; To jack in, run "slimerepl" from bash, then
 ;; `slime-connect` to `localhost:5555`.
 
-(defun ch-char (char_ x y)
-  (charms:write-string-at-point charms:*standard-window* char_ x y))
+(defparameter *world-size* 100)
 
-(defmacro with-game-loop (w h &rest body)
-  `(charms:with-curses ()
-     (charms:disable-echoing)
-     (multiple-value-bind
-           (,w ,h)
-         (charms:window-dimensions charms:*standard-window*)
-       ,@body
-       (loop named poll-kbd
-          do (progn
-               (when (eql (charms:get-char charms:*standard-window*) #\q)
-                 (return-from poll-kbd))
-               (sleep 0.01))))))
+(defun make-world ()
+  (make-array (list *world-size* *world-size*)
+              :element-type 'character
+              :initial-element #\space
+              :adjustable t))
 
-(defun write-chars-and-poll ()
-  (with-game-loop
-      width height
-      (loop repeat 10
-         do (let ((x (random width))
-                  (y (random height)))
-              (ch-char (rand-nth '(";" "\"" "'")) x y)
-              (charms:refresh-window charms:*standard-window*)))
-      (ch-char "@" (floor (/ width 2)) (floor (/ height 2)))))
+(defvar *world* (make-world))
+(defparameter *their-x* 10)
+(defparameter *their-y* 10)
+
+(defun initialize-world ()
+  (loop repeat 50
+     do (let* ((x (random *world-size*))
+               (y (random *world-size*))
+               (ch (rand-nth '(#\^ #\" #\' #\. #\# #\*))))
+          (setf (aref *world* x y) ch))))
+
+(defmacro with-screen-dims (w h &rest body)
+  `(multiple-value-bind
+         (,w ,h)
+       (charms:window-dimensions charms:*standard-window*)
+     ,@body))
+
+(defun draw-world (w h)
+  (loop for x from 0 below w
+     do (loop for y from 0 below h
+           do
+             (if (and (< x w)
+                      (< x *world-size*)
+                      (< y h)
+                      (< y *world-size*))
+                 (charms:write-char-at-point
+                  charms:*standard-window*
+                  (aref *world* x y) x y))))
+  (charms:write-char-at-point charms:*standard-window*
+                              #\@
+                              *their-x*
+                              *their-y*)
+  (charms/ll:curs-set 0))
+
+(defun movdel (w h dx dy)
+  (setf *their-x* (min (1- w) (max 0 (+ dx *their-x*))))
+  (setf *their-y* (min (1- h) (max 0 (+ dy *their-y*)))))
+
+(defun poll-till-done ()
+  (loop named poll-kbd
+     do (progn
+          (with-screen-dims w h
+            (draw-world w h)
+            (case (charms:get-char charms:*standard-window*)
+              (#\q (return-from poll-kbd))
+              (#\h (movdel w h -1 0))
+              (#\l (movdel w h 1 0))
+              (#\k (movdel w h 0 -1))
+              (#\j (movdel w h 0 1))
+              (#\y (movdel w h -1 -1))
+              (#\n (movdel w h -1 1))
+              (#\i (movdel w h 1 -1))
+              (#\m (movdel w h 1 1))))
+          (sleep 0.01))))
+
+(defmacro with-charms (&rest body)
+  `(charms:with-curses () ,@body))
+
+(defun show-game (&key poll)
+  (with-charms
+    (charms:clear-window charms:*standard-window*)
+    (charms:disable-echoing)
+    (charms:enable-raw-input :interpret-control-characters t)
+    (charms:enable-extra-keys charms:*standard-window*)
+    (with-screen-dims w h (draw-world w h))
+    (when poll (poll-till-done))))
+
+;;(floor (/ width 2))
 
 (defun main (&rest args)
-  (write-chars-and-poll))
+  (setf *random-state* (make-random-state t))
+  (initialize-world)
+  (show-game :poll t))
+
+(defmacro comment (&rest body))
+(comment
+ (main)
+
+ )
